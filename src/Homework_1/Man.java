143c652dd3c9bd99ccc49991352ae331bb2365b6
kaifler <96072701+kaifler@users.noreply.github.com>
2022-06-08 22:17:48 +0500

    Homework 1 Core

--- src/Homework_1/Man.java
+++ src/Homework_1/Man.java
@@ -1,2 +1,13 @@
-package Homework_1;public class Man {
+package Homework_1;
+
+public class Man {
+    private String name;
+    private int skillSwimm;
+
+    public Man (String name, int skillSwimm) {
+        this.name = name;
+        this.skillSwimm = skillSwimm;
+    }
+    public String getName() {return name; }
+    public int getSkillSwimm() {return skillSwimm; }
 }
